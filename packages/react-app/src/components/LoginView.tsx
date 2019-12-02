6bfc292f451fd25321dbe7d527e58993a61d70e5
Tom Medema <tommedema@gmail.com>
2019-12-02 12:49:10 -0800

    login with popup instead of redirect

--- packages/react-app/src/components/LoginView.tsx
+++ packages/react-app/src/components/LoginView.tsx
@@ -2,11 +2,10 @@ import React, { Fragment, EventHandler, SyntheticEvent } from 'react'
 import { useAuth0 } from '../lib/auth0'
 
 const LoginView: React.FC = () => {
-  const { loginWithRedirect, loginWithPopup } = useAuth0()
+  const { loginWithPopup } = useAuth0()
 
   const onLoginClick: EventHandler<SyntheticEvent<HTMLAnchorElement>> = async (e) => {
     e.preventDefault()
-    // await loginWithRedirect()
     await loginWithPopup()
   }
 
